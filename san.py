a7f82747a38fb2e5503ea0c6acd6f2d9b9940019
Sanjay <sanjuedwin4@gmail.com>
2024-10-23 01:36:10 +0530

    Update san.py

--- san.py
+++ san.py
@@ -4,3 +4,5 @@ print("my college name")
 print("Home science college Hassan")
 print ("My native village is magge")
 print ("My School name is Saint Francis Xavier School Magge")
+print ("my college name is ")
+Saint Joseph's PU college Hassaan
