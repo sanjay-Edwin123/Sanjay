e4cb7b2b44e3f485a6d0de919731b130ec061c43
Sanjay <sanjuedwin4@gmail.com>
2024-10-23 01:34:14 +0530

    Update san.py

--- san.py
+++ san.py
@@ -2,3 +2,5 @@ print("my name is sanjay")
 I am a Devops Learner
 print("my college name")
 print("Home science college Hassan")
+print ("My native village is magge")
+print ("My School name is Saint Francis Xavier School Magge")
