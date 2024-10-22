b453d462b0cffb30494dfd8e10a56606dfe51736
sanjay <sanjay@gmail.com>
2024-10-23 02:06:12 +0530

    commited successfully

--- san.py
+++ san.py
@@ -6,3 +6,8 @@ print ("My native village is magge")
 print ("My School name is Saint Francis Xavier School Magge")
 print ("my college name is ")
 Saint Joseph's PU college Hassaan
+
+
+a=10
+b=20
+print=(a+b)
